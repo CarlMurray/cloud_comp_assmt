2cf9ff87f32633438136bf7ec35ca08dd3de1c37
CarlMurray <carl.j.murray@gmail.com>
2025-02-16 23:51:55 +0000

    Add restart always policy to apache container deployment script

--- ec2_files/scripts/deployApacheDockerContainer.sh
+++ ec2_files/scripts/deployApacheDockerContainer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name apache-container -d -p 80:80 -p 443:443 -v /home/ubuntu/cloud_comp_assmt/ec2_files:/usr/local/apache2/htdocs httpd
+docker run --name apache-container -d -p 80:80 -p 443:443 -r always -v /home/ubuntu/cloud_comp_assmt/ec2_files:/usr/local/apache2/htdocs httpd
